26e9d15ce2046f91c3572d5250ca4e12280ddda8
VintorezVS <vintorezvs@gmail.com>
2014-12-27 00:18:58 +0200

    Поменял структуру списков скилов и проектов. Настроил их вывод для пользователя.

--- front/users/controllers.js
+++ front/users/controllers.js
@@ -1,16 +1,6 @@
-function getProjectsByID(projectID, projects) {
-    for(var i = 0; i < projects.length; i++)
-        if (projectID == projects[i].id) return projects[i];
-}
-
-function getSkillByID(id, skills) {
-    var idArray = id.split(".");
-    return getSkillByIDRec(idArray, 0, skills);
-}
-
-function getSkillByIDRec(id, index, skills) {
-    if (index == id.length - 1) return skills[parseInt(id[index]) - 1];
-    return getSkillByIDRec(id, index + 1, skills[parseInt(id[index]) - 1].subskills);
+function getObjectByID(objectID, objects) {
+    for(var item in objects)
+        if (objectID == item) return objects[item];
 }
 
 angular.module('skills').controller('users_show_controller',function($scope,$http,$routeParams){
@@ -20,15 +10,15 @@ angular.module('skills').controller('users_show_controller',function($scope,$htt
             $scope.skills = skills;
             $http.get('/models/users/' + $routeParams.user_id + '.json').success(function (user) {
                 $scope.user = user;
-
+                //Копирует скилы по ID из общего списка проектов в список конкретного юзера
                 $scope.userSkills = [];
                 for (var id in $scope.user.skills)
-                    $scope.userSkills.push(getSkillByID($scope.user.skills[id], $scope.skills));
+                    $scope.userSkills.push(getObjectByID($scope.user.skills[id], $scope.skills));
 
                 //Копирует проекты по ID из общего списка проектов в список конкретного юзера
-                $scope.user.projects = [];
+                $scope.userProjects = [];
                 for (var pID in $scope.user.projectsID)
-                    $scope.user.projects.push(getProjectsByID($scope.user.projectsID[pID], $scope.projects));
+                    $scope.userProjects.push(getObjectByID($scope.user.projectsID[pID], $scope.projects));
             }).error(function () {
                 $scope.user = null;
             });
