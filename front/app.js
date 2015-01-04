98b95937ebf87133c999e029f4bb79ad3eed7574
ma140585 <ma14051985@gmail.com>
2015-01-04 17:56:14 +0200

    подкорректировал файл app.js = прописал маршруты к ШАБЛОНАМ из файла index.html

--- front/app.js
+++ front/app.js
@@ -6,5 +6,15 @@ app.config(function($locationProvider,$routeProvider){
     $routeProvider
         .when('/', {templateUrl: '/front/main.html'})
         .when('/users/:user_id', {templateUrl: '/front/users/show.html', controller:'users_show_controller'})
+        .when('/my_page', {templateUrl:'/front/profile.html'})
+        .when('/all_users', {templateUrl:'/front/profile_viewer_all.html'})
+        .when('/all_skills', {templateUrl:'/front/skills_viewer_all.html'})
+        .when('/all_job', {templateUrl:'/front/job_viewer_all.html'})
+        .when('/decision', {templateUrl:'/front/job_decision.html'})
+        .when('/approve', {templateUrl:'/front/job_approve.html'})
+        .when('/check', {templateUrl:'/front/job_check.html'})
+        .when('/creat', {templateUrl:'/front/job_create.html'})
+        .when('/view_one', {templateUrl:'/front/job_viewer_one.html'})
         .otherwise({redirectTo: '/'});
 });
+
